c24b3e138407ef0784d186c85ab1eee88e279c32
Keyner <ktrillos2@gmail.com>
2024-02-06 14:41:41 -0500

    scrappy pagina inmuebles

---
+++ app/api/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export async function POST(request: Request) {
+	const { searchParams: userSearch } = await request.json();
+
+	if (!userSearch) {
+		return NextResponse.json(
+			{ error: "Please provide a search query" },
+			{ status: 400 }
+		);
+	}
+
+	let browser;
+	try {
+		browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		await page.goto("https://pitaibizainmobiliaria.com.co/inmuebles/");
+		let data:any = [];
+		let lastPageText:any = '';
+
+		do {
+			const pageData = await page.$$eval("div.card-in-list", (cards) =>
+				cards.map((card) => {
+					const h4Text = card.querySelector("h4")?.textContent;
+					const pText = card.querySelector("div > p")?.textContent;
+					const spanText = card.querySelector("div.img > span")?.textContent;
+					return { h4: h4Text, p: pText, span: spanText };
+				})
+			);
+			data = [...data, ...pageData];
+			const nextPage = await page.waitForSelector("#siguienteA");
+			const lastPage = await page.waitForSelector("#finalA");
+			lastPageText = await page.evaluate((el) => el?.textContent, lastPage);
+			await Promise.all([
+				nextPage?.click(),
+				page.waitForNavigation({ waitUntil: 'networkidle0' }),
+			]);
+		} while (!page.url().includes("2"));
+
+		await page.close();
+
+		return NextResponse.json({ data }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Something went wrong" },
+			{ status: 200 }
+		);
+	} finally {
+		if (browser) {
+			await browser.close();
+		}
+	}
+}
---
+++ app/page.tsx
@@ -0,0 +1,72 @@
+// Archivo: page.tsx
+"use client";
+import {
+	Button,
+	Input,
+	Table,
+	TableBody,
+	TableCell,
+	TableColumn,
+	TableHeader,
+	TableRow,
+} from "@nextui-org/react";
+import { useState } from "react";
+
+interface Inmuebles {
+	h4: string;
+	p: string;
+	span: string;
+}
+
+export default function Home() {
+	const [inputValue, setInputValue] = useState("");
+	const [inmuebles, setInmuebles] = useState<Inmuebles[]>();
+
+	const getInfo = async () => {
+		const response = await fetch("/api", {
+			method: "POST",
+			body: JSON.stringify({ searchParams: inputValue }),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await response.json();
+		setInmuebles(data.data);
+		console.log(data);
+		console.log(data.data, "datadataaaa");
+	};
+
+	return (
+		<main className="flex flex-col gap-3 items-center justify-center !max-h-screen">
+			<div className="flex flex-row items-center justify-center gap-5 my-5">
+			<Input
+				className="w-1/6"
+				type="text"
+				value={inputValue}
+				onChange={(event) => setInputValue(event.target.value)}
+			/>
+			<Button onClick={getInfo}>Buscar</Button>
+			</div>
+			{inmuebles && (
+				<div className="!max-w-1/2 !max-h-[600px]">
+					<Table isHeaderSticky aria-label="Example static collection table">
+						<TableHeader>
+							<TableColumn>Nombre</TableColumn>
+							<TableColumn>Ubicación</TableColumn>
+							<TableColumn>Precio</TableColumn>
+						</TableHeader>
+						<TableBody>
+							{inmuebles.map((item, index) => (
+								<TableRow key={index}>
+									<TableCell>{item.h4}</TableCell>
+									<TableCell>{item.p}</TableCell>
+									<TableCell>{item.span}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
+			)}
+		</main>
+	);
+}
