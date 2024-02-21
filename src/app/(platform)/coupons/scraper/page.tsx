4fe33f94a19c2a8a70fa38ae285de3fd598745f6
Keyner <ktrillos2@gmail.com>
2024-02-21 12:19:28 -0500

    ño se pq

--- src/app/(platform)/coupons/scraper/page.tsx
+++ src/app/(platform)/coupons/scraper/page.tsx
@@ -1,10 +1,21 @@
+'use client'
+import { useState, useEffect } from 'react';
 import { LoggerComponent } from "@/components";
 import { CouponsScraperTable } from "@/modules";
 import { generalService } from "@/service";
+import { LogMessage } from '@/interfaces';
 
-export default async function CouponsScraperPage() {
-  
-  const messages = await generalService.getLogMessagesByCategory("COUPON");
+export default function CouponsScraperPage() {
+  const [messages, setMessages] = useState<LogMessage[]>([]);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const result = await generalService.getLogMessagesByCategory("COUPON");
+      setMessages(result);
+    };
+
+    fetchMessages();
+  }, []);
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
