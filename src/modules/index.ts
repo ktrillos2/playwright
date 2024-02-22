c8389661fdacd79136fe760e9d0b0458c21bd3cd
Keyner <ktrillos2@gmail.com>
2024-02-22 10:37:21 -0500

    cambio de exportacion

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,5 @@
 export * from './coupons/CouponsTable';
 export * from './coupons/DeleteCouponsButton';
-export * from './coupons/coupon-editor/CouponImageEditor';
 export * from './coupons/scraper/CouponsScraperTable';
 export * from './inmuebles/DeleteInmueblesButton';
 export * from './inmuebles/InmueblesTable';
