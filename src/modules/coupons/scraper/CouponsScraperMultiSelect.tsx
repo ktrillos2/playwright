64720e07e93a409f3242e180705471e57969c861
Cris <crisvera1909@gmail.com>
2024-02-27 18:01:33 -0500

    ni idea que hice

--- src/modules/coupons/scraper/CouponsScraperMultiSelect.tsx
+++ src/modules/coupons/scraper/CouponsScraperMultiSelect.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { LogCategory, LogType } from "@/interfaces";
+import { generalService } from "@/service";
 import { Button, Image, Select, SelectItem } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const CouponsScraperMultiSelect = () => {
   const [platform, setPlatform] = useState<any>([]);
+  const [category, setCategory] = useState<any>([]);
   const platforms = [
     {
       name: "Exito",
@@ -14,6 +18,28 @@ export const CouponsScraperMultiSelect = () => {
       image: "/page-images/metro-logo.png",
     },
   ];
+
+  const categories = [
+    { name: "Tecnologia", value: "/tecnologia" },
+    { name: "Celulares", value: "/tecnologia/celulares" },
+    { name: "Vinos y licores", value: "/vinos-y-licores" },
+  ];
+
+  const scrape=async()=>{
+    console.log(category)
+    if(category.size===0){
+      return toast.error("La categoría es obligatoria")
+    }
+    await generalService.createLogMessage({
+      category: LogCategory.COUPON,
+      type: LogType.LOADING,
+      message: "Scrapeando Exito",
+    });
+    const params={
+      category:category
+    }
+    await generalService.scrapeExito(params);
+  }
 
   return (
     <div className="w-full flex justify-center items-center gap-4">
@@ -40,27 +66,24 @@ export const CouponsScraperMultiSelect = () => {
       {platform.size > 0 && (
         <>
           <Select
-            items={platforms}
+            items={categories}
             label="Selecciona la categoría"
             className="w-1/6"
             variant="bordered"
+            selectedKeys={category}
+        onSelectionChange={setCategory}
           >
-            {(platform) => (
-              <SelectItem key={platform.name} textValue={platform.name}>
+            {(category) => (
+              <SelectItem key={category.value} textValue={category.name}>
                 <div className="flex gap-2 items-center">
-                  <Image
-                    src={platform.image}
-                    alt="image"
-                    className="w-12"
-                  ></Image>
                   <div className="flex flex-col">
-                    <span className="text-small">{platform.name}</span>
+                    <span className="text-small">{category.name}</span>
                   </div>
                 </div>
               </SelectItem>
             )}
           </Select>
-          <Button>Scrapear</Button>
+          <Button onClick={scrape}>Scrapear</Button>
         </>
       )}
     </div>
