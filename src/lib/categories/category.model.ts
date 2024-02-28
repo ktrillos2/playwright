0968ec332f44f08c154f4739cb589fab5373802b
Cris <crisvera1909@gmail.com>
2024-02-28 12:56:15 -0500

    subiendo cambios

--- src/lib/categories/category.model.ts
+++ src/lib/categories/category.model.ts
@@ -1,10 +1,20 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { PaginateModel, Schema } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
 import { DBCategory } from "@/interfaces";
 
-const categorySchema = new Schema<DBCategory>({
+interface CategoryDocument extends Document, DBCategory {}
+
+const categorySchema = new Schema<CategoryDocument>({
   name: { type: String, required: true },
   slug: { type: String, required: true },
 });
 
-export const CategoryModel = mongoose.model("Category", categorySchema);
+categorySchema.plugin(mongoosePaginate);
+
+export const CategoryModel: PaginateModel<CategoryDocument> =
+  (mongoose.models.Category as PaginateModel<CategoryDocument>) ||
+  mongoose.model<CategoryDocument, PaginateModel<CategoryDocument>>(
+    "Coupon",
+    categorySchema
+  );
