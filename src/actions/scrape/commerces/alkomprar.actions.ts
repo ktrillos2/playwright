dbbd713af4a3d5a0a0168a5e7befd5f46a5232ee
Lorena0521 <lorbalagera26@gmail.com>
2024-04-15 14:21:14 -0500

    guardando

--- src/actions/scrape/commerces/alkomprar.actions.ts
+++ src/actions/scrape/commerces/alkomprar.actions.ts
@@ -1,6 +1,6 @@
 import { DBCoupon, LogType } from "@/interfaces";
 import { Browser } from "playwright";
-import { logger } from "../helpers";
+import { logger, sleep } from "../helpers";
 import { autoScroll } from "@/helpers";
 
 export interface ScrapePageProps {
@@ -12,21 +12,25 @@ export type CouponScraped = Omit<DBCoupon, "commerce" | "category">;
 
 export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps) => {
   let products: CouponScraped[] = [];
-  
-  try {
 
+  try {
     //* Navega a la página
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: "networkidle" });
-        
+    await page.goto(url, { waitUntil: "load" });
+    await sleep(1000);
+    await autoScroll(page);
+
     //* Obtener botón para cargar más productos
     for (let i = 0; i < 5; i++) {
       const buttonFired = await page.$(".ais-InfiniteHits-loadMore");
-    await buttonFired?.click();
-    await autoScroll(page); 
+      await buttonFired?.click();
+      await autoScroll(page);
+    }
 
-   
-     const allElement = await page.$$eval(".promotedProduct", (promotedProduct) =>{
+    const allElement = await page.$$eval(
+      ".promotedProduct",
+      (promotedProduct) =>
+        promotedProduct.map((product: Element) => {
           const convertToNumber = (item: string | null | undefined) => {
             if (item) {
               const numericValue = item.replace(/[^\d]/g, "");
@@ -35,20 +39,19 @@ export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps) => {
               return 0;
             }
           };
-         return  promotedProduct.map((product: Element) => {
-
-           const linkElement = product.querySelectorAll(
-            ".alk-container-items-category"
+          const linkElement = product.querySelectorAll(
+            ".product__item__top__title"
           ) as NodeListOf<HTMLAnchorElement>;
           let linkTitle = "";
           let linkHref = "";
           if (linkElement.length > 0) {
             linkTitle = linkElement[1].title;
             linkHref = linkElement[1].href;
-          } 
-        
-               
-          const img = product.querySelector(".product__item__information__image");
+          }
+
+          const img = product.querySelector(
+            ".product__item__information__image"
+          );
 
           let imgURL = "";
           if (img) {
@@ -58,52 +61,36 @@ export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps) => {
           const priceWithDiscount = product.querySelector(
             ".product__price--discounts__old"
           );
-           const priceWithoutDiscount = product.querySelector(
-            ".price"
-          );
+          const priceWithoutDiscount = product.querySelector(".price");
 
           const priceWithCard = product.querySelector(".price-contentPlp");
-          const discount = product.querySelector(
-            'span[.product__item__information__c20m__values]'
-          );
+          const discount = 0;
           const brandName = product.querySelector(
             ".product__item__information__brand"
           );
-        
-        
+
           const body: CouponScraped = {
             name: linkTitle,
             url: linkHref,
             images: imgURL ? [imgURL] : [],
             lowPrice: convertToNumber(priceWithDiscount?.textContent),
             discountWithCard: convertToNumber(priceWithCard?.textContent),
-            discountPercentage: convertToNumber(discount?.textContent),
+            discountPercentage: 0,
             brandName: brandName?.textContent,
             priceWithoutDiscount: convertToNumber(
               priceWithoutDiscount?.textContent
             ),
-          }
+          };
           return body;
         })
-        
-     }); 
-        
-           products = products.concat(allElement);
+    );
 
-      const nextButton = await page.$(".ais-InfiniteHits-loadMore");
+    console.log("salgo: " + allElement);
 
-      if (nextButton) {
-        await nextButton.dispatchEvent("click");
-      } else {
-        break;
-      }
-  
-    }
-
+    products = allElement;
     if (!products.length)
       throw new Error("No se encontraron productos en el scrapeo");
-
-  }catch (error: any) {
+  } catch (error: any) {
     await logger(
       LogType.ERROR,
       error?.message ?? "No se pudo scrapear Alkomprar",
@@ -117,5 +104,4 @@ export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps) => {
     }
     return products;
   }
-}
-
+};
--- src/actions/scrape/helpers/browser.helpers.ts
+++ src/actions/scrape/helpers/browser.helpers.ts
@@ -5,24 +5,5 @@ export const getBrowser = async () => {
     headless: false,
   });
 
-<<<<<<< HEAD
-	const browser = await playwright.chromium.launch({
-		// args: [
-		// 	"--disable-setuid-sandbox",
-		// 	"--no-sandbox",
-		// 	"--single-process",
-		// 	"--no-zygote",
-		// ],
-		 headless: false,
-		// executablePath:
-		// 	process.env.NODE_ENV === "production"
-		// 		? process.env.PUPPETEER_EXECUTABLE_PATH
-		// 		: locateBrowser,
-	});
-
-	return browser;
-};
-=======
   return browser;
 };
->>>>>>> main
