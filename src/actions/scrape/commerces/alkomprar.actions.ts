e49366e6da4f6567ec0d893013f514ffc99c3887
Lorena0521 <lorbalagera26@gmail.com>
2024-04-16 16:05:30 -0500

    guardando

--- src/actions/scrape/commerces/alkomprar.actions.ts
+++ src/actions/scrape/commerces/alkomprar.actions.ts
@@ -14,104 +14,93 @@ export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps) => {
   let products: CouponScraped[] = [];
 
   try {
+
     //* Navega a la página
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "load" });
     await sleep(1000);
     await autoScroll(page);
 
+
     //* Obtener botón para cargar más productos
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 5; i++) {
       const buttonFired = await page.$(".ais-InfiniteHits-loadMore");
       await buttonFired?.click();
       await autoScroll(page);
     }
 
-    // esperar a que los elementos carguen
-    await page.waitForSelector(".promotedProduct");
-
-const section: any;
-    const allElement = await page.$$eval(section=> {
-      const promotedProduct = document.querySelectorAll('.promotedProduct');
-
-      const convertToNumber = (item: string | null | undefined): number => {
-        // logica para convertir de string a numero
-        if (item === null || item === undefined) {
-            return 0; 
-        }
-        return parseInt(item); 
-    };
-    
-      promotedProduct.forEach((product: Element) => {
-        const nameElement = product.querySelectorAll(
-          ".product__item__top__title"
-        );
-
-        console.log(nameElement);
-        const linkElement = product.querySelectorAll(
-          ".alk-grid-items-category"
-        ) as NodeListOf<HTMLAnchorElement>;
-        if (linkElement.length > 0) {
-          console.log(linkElement);
-        }
-
-
-        const img = product.querySelector(".product__item__information__image");
-        console.log(img);
-
-        let imgURL = "";
-        if (img) {
-          imgURL = img.getAttribute("src") ?? "";
-        }
-
-        const priceWithDiscount = product.querySelector(
-          ".product__price--discounts__old"
-        );
-        console.log(priceWithDiscount);
-
-        const priceWithoutDiscount = product.querySelector(".price");
-
-        const priceWithCard = product.querySelector(".price-contentPlp");
-        const discount = 0;
-        const brandName = product.querySelector(
-          ".product__item__information__brand"
-        );
-
-        const body: CouponScraped = {
-          name: convertToNumber(nameElement?.textContext),
-          url: "0",
-          images: imgURL ? [imgURL] : [],
-          lowPrice: convertToNumber(priceWithDiscount?.textContent),
-          discountWithCard: convertToNumber(priceWithCard?.textContent),
-          discountPercentage: 0,
-
-          brandName: brandName?.textContent,
-          priceWithoutDiscount: convertToNumber(
-            priceWithoutDiscount?.textContent
-          ),p
+      const allElement = await page.$$eval(".promotedProduct", (promotedProduct) => {
+        const convertToNumber = (item: string | null | undefined) => {
+          if (item === null || item === undefined) {
+            return 0;
+          }
+          return parseInt(item);
         };
-        return body;
-      })
-    });
 
-    console.log(allElement);
+        return promotedProduct.map((product: any) => {
 
-    if (!allElement.length)
-      throw new Error("No se encontraron productos en el scrapeo");
-    const promotedProduct = document.querySelectorAll('.promotedProduct');
+          const nameElement = product.querySelector(
+            ".product__item__top__title"
+          );
+          const linkElement = product.querySelectorAll(
+            ".alk-container-items-category"
+          ) as HTMLAnchorElement | null;
 
-  } catch (error: any) {
-    await logger(
-      LogType.ERROR,
-      error?.message ?? "No se pudo scrapear Alkomprar",
-      error
-    );
 
-    throw new Error(error.message);
-  } finally {
-    if (browser) {
-      await browser.close();
+
+          const img = product.querySelector(".product__item__information__image");
+
+          let imgURL = "";
+          if (img) {
+            imgURL = img ? img.getAttribute("src") ?? "" : "";;
+          }
+
+          const priceWithDiscount = product.querySelector(
+            ".product__price--discounts__old"
+          );
+          const priceWithoutDiscount = product.querySelector(
+            ".price"
+          );
+
+          const priceWithCard = product.querySelector(".price-contentPlp");
+          const discount = 0;
+
+          const brandName = product.querySelector(
+            ".product__item__information__brand"
+          );
+
+
+          const body: CouponScraped = {
+            name: nameElement?.textContent ?? "",
+            url: linkElement ? linkElement.href : "",
+            images: imgURL ? [imgURL] : [],
+            lowPrice: convertToNumber(priceWithDiscount?.textContent),
+            discountWithCard: convertToNumber(priceWithCard?.textContent),
+            discountPercentage: 0,
+            brandName: brandName?.textContent,
+            priceWithoutDiscount:  convertToNumber(priceWithoutDiscount?.textContent),
+          }
+          return body;
+        })
+      });
+      console.log(allElement);
+
+      if (allElement.length == 0)
+        throw new Error("No se encontraron productos en el scrapeo");
+       products = allElement;
+
+    }catch (error: any) {
+      await logger(
+        LogType.ERROR,
+        error?.message ?? "No se pudo scrapear Alkomprar",
+        error
+      );
+
+      throw new Error(error.message);
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+      return products;
     }
-    return products;
-  }
-};
+  };
