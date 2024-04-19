79cd238d747d643de8cfff051e71257408cad888
Lorena0521 <lorbalagera26@gmail.com>
2024-04-18 19:20:09 -0500

    guardando

--- src/actions/scrape/commerces/alkomprar.actions.ts
+++ src/actions/scrape/commerces/alkomprar.actions.ts
@@ -1,162 +1,80 @@
 import { DBCoupon, LogType } from "@/interfaces";
-import { Browser, selectors } from "playwright";
-import { logger, sleep } from "../helpers";
+import { Browser } from "playwright";
+import { logger, sleep,  } from "../helpers";
 import { autoScroll } from "@/helpers";
-
 
 export interface ScrapePageProps {
   browser: Browser;
   url: string;
 }
 
-export type CouponScraped = Omit<DBCoupon, "commerce" | "category">;
+export type CouponScraped = Omit<DBCoupon, "commerce" | "category">& { images?: string[] };
 
-export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps) => {
+export const scrapeAlkomprar = async ({ browser, url }: ScrapePageProps): Promise<CouponScraped[]> => {
   let products: CouponScraped[] = [];
 
-  try {
 
-    //* Navega a la página
+  try {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "load" });
     await sleep(1000);
     await autoScroll(page);
-    console.log(0)
 
-    // Obtener botón para cargar más productos 
+    //iterar el boton de cargar mas productos
     for (let i = 0; i < 5; i++) {
       const buttonFired = await page.$(".ais-InfiniteHits-loadMore");
       await buttonFired?.click();
       await autoScroll(page);
-      console.log(1)
     }
+    //esperar a que carguen los elementos 
+    await page.waitForSelector(".ais-InfiniteHits-item");
 
+    // evaluar agregar los datos a CouponScraped[]
+    products = await page.$$eval(".ais-InfiniteHits-item", (elements) => {
+      return elements.map(product => {
 
-    const allElement = await page.waitForSelector(".promotedProduct");
-    const productList = products;
-    const product = await page.$$eval(".promotedProduct", (product) => {
+        //obtener los datos 
+        const nameElement = product.querySelector(".product__item__top__title");
+        const linkElement = product.querySelector<HTMLAnchorElement>('.product__item__top__title a');         // HTMLAnchorElement= obtener dentro de cada elemento de producto un enlace `<a>
+        const image = product.querySelector(".product__item__information__image");
+        const priceWithDiscount = product.querySelector(".product__price--discounts__old");
+        const priceWithoutDiscount = product.querySelector(".price");
+        const priceWithCard = product.querySelector(".price-contentPlp");
+        const brandName = product.querySelector(".product__item__information__brand");
 
+// calcilar el porcentaje de cada producto
 
-      if (product) {
-        const productNames = productList.map(product => product.name);
-        console.log(productNames);
-      } else {
-        console.log("codigo no funciona");
-      };
+//convertir los datos de string o null a numero
+        const convertToNumber = (item: string | null) => {
+          if (item) {
+            const numericValue = item.replace(/[^\d]/g, "");
+            return parseInt(numericValue, 10);
+          } else {
+            return 0;
+          }
+        };
 
-
-
-
-      const nameElement = product.querySelector(
-        ".product__item__top__title"
-      );
-      console.log(nameElement, 11111)
-
-      const linkElement: NodeListOf<Element> = product.querySelectorAll("a.skiptocontent");
-
-      let link: string | null = null;
-
-      if (linkElement.length > 0) {
-        const firstElement = linkElement[0];
-        // Accedemos al atributo href del elemento
-        link = firstElement.getAttribute("href");
-      } else {
-
-      }
-
-      console.log(link, "44444");
-
-
-
-      // verificar que no sea undefine o null y crear la lista de elementos encontrados
-      const image: Element | null = product.querySelector(".product__item__information__image");
-
-      let imagesElements: NodeListOf<Element>;
-
-      if (image) {
-        imagesElements = document.querySelectorAll(".product__item__information__image");
-      } else {
-        imagesElements = document.querySelectorAll(""); // Esto devolverá una NodeList vacía
-      }
-      const images: string[] = [];
-
-      imagesElements.forEach((img) => {
-        if (img) {
-          images.push(img.getAttribute("src")!);
-        }
+        return {
+          name: nameElement ? nameElement.textContent!.trim() : "", 
+          url: linkElement ? linkElement.href : "",
+          image: image ? image.getAttribute("src")! : "",
+          lowPrice: convertToNumber(priceWithDiscount ? priceWithDiscount.textContent : ""),
+          discountWithCard: convertToNumber(priceWithCard ? priceWithCard.textContent : ""),
+          brandName: brandName ? brandName.textContent!.trim() : "",
+          priceWithoutDiscount: convertToNumber(priceWithoutDiscount ? priceWithoutDiscount.textContent : ""),
+          discountPercentage: 0,
+          images: [],
+        };
       });
-      console.log(images, 7777777)
-
-
-
-
-      const priceWithDiscount = product.querySelector(
-        ".product__price--discounts__old"
-      );
-      console.log(priceWithDiscount, "55655126")
-
-
-
-      const priceWithoutDiscount = product.querySelector(
-        ".price"
-      );
-      console.log(priceWithoutDiscount, 55555)
-
-
-
-      const priceWithCard = product.querySelector(".price-contentPlp"
-      ); console.log(6)
-
-
-
-      const discount = 0;
-      console.log(discount);
-
-
-
-      const brandName = product.querySelector(
-        ".product__item__information__brand"
-      ); 'NodeListOf<Element>'
-      console.log(brandName)
-
-
-      return {
-        name: nameElement ? nameElement.textContent! : "",
-        url: linkElement ? "https://www.alkomprar.com/celulares/smartphones/c/BI_101" +
-          linkElement
-          : "",
-        images,
-        lowPrice: convertToNumber(priceWithDiscount ? priceWithDiscount.textContent! : ""),
-        discountWithCard: convertToNumber(priceWithCard ? priceWithCard.textContent! : ""
-        ),
-        brandName: "",
-        priceWithoutDiscount: convertToNumber(priceWithoutDiscount ? priceWithoutDiscount.textContent! : ""
-        ),
-        discountPercentage: 0,
-
-      }
     });
+    console.log(products);
+  } catch (error: any) {
+    await logger(LogType.ERROR, error?.message ?? "No se pudo scrapear Alkomprar", error);
+    throw new Error(error?.message ?? "No se pudo scrapear Alkomprar");
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+    return products;
   }
-  }
-  console.log(allElement)
-  console.log(product)
-
-
-
-
-} catch (error: any) {
-  await logger(
-    LogType.ERROR,
-    error?.message ?? "No se pudo scrapear Alkomprar",
-    error
-
-  );
-
-  throw new Error(error.message);
-} finally {
-  if (browser) {
-    await browser.close();
-  }
-  return products;
-}
-  }
+};
