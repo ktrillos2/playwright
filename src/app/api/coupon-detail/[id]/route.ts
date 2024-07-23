b214a1cdcf5e920c366e0a53c09cc5c88e5a13b1
Keyner <ktrillos2@gmail.com>
2024-07-23 12:05:46 -0500

    validacion de coupon detail

--- src/app/api/coupon-detail/[id]/route.ts
+++ src/app/api/coupon-detail/[id]/route.ts
@@ -7,6 +7,7 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
 
   try {
     const couponDetail = await couponDetailActions.getCouponDetailById(id as string);
+    if (!couponDetail) throw new Error("errors.coupon-detail-not-found");
     return new NextResponse(JSON.stringify(couponDetail), { status: 200 });
   } catch (error: any) {
     return new NextResponse(error.message, { status: 500 });
