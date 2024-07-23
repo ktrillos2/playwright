273454bc723fb67a126c17ff4af1ac5b24241fd5
Keyner <ktrillos2@gmail.com>
2024-07-23 12:22:44 -0500

    cors :)

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,19 @@ const nextConfig = {
     config.externals = [...config.externals, { canvas: 'canvas' }]
     return config;
   },
+  async headers() {
+    return [
+        {
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
 };
 
 export default nextConfig;
